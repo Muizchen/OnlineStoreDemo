c1ef41b5337d50bc83a7cb59b8086e505a5f77d8
Muizchen <muizchen@hotmail.com>
2019-12-04 17:59:52 +0800

    feat(BatteryAddress): 增加蓄电池地址搜索地图功能

--- TuhuSample/AddressSelection/BatteryAddressSearchViewController.swift
+++ TuhuSample/AddressSelection/BatteryAddressSearchViewController.swift
@@ -7,11 +7,16 @@
 //
 
 import Foundation
+import MapKit
 
 class BatteryAddressSearchViewController: UIViewController {
     
     @IBOutlet weak var searchBar: UISearchBar!
+    @IBOutlet weak var mapView: MKMapView!
     @IBOutlet weak var tableView: UITableView!
+    
+    @IBOutlet weak var tableViewLeadingConstraint: NSLayoutConstraint!
+    @IBOutlet weak var tableViewHeightConstraint: NSLayoutConstraint!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,16 +24,23 @@ class BatteryAddressSearchViewController: UIViewController {
         tableView.registerNibOf(BatteryAddressInputCell.self)
         tableView.registerNibOf(BatteryAddressSearchCell.self)
         
-        let searchField = searchBar.value(forKey: "_searchField") as? UITextField
+        var searchField: UITextField?
+        if #available(iOS 13, *) {
+            searchField = searchBar.searchTextField
+        } else {
+            searchField = searchBar.value(forKey: "searchField") as? UITextField
+        }
         searchField?.font = UIFont.systemFont(ofSize: 14)
         searchField?.textColor = UIColor.tuhu.textBlack
         searchField?.attributedPlaceholder = NSAttributedString.init(string: "请输入详细地址", attributes: [NSAttributedString.Key.font: UIFont.systemFont(ofSize: 14), NSAttributedString.Key.foregroundColor: UIColor.tuhu.textLightGray])
         searchField?.borderStyle = .none
-        searchField?.leftView = nil
-        searchBar.searchTextPositionAdjustment = UIOffset.init(horizontal: 2, vertical: 0)
+        /// 左侧搜索图标
+        searchField?.leftViewRect(forBounds: CGRect(x: 0, y: 0, width: 14, height: 14))
+        searchBar.setPositionAdjustment(UIOffset(horizontal: -6, vertical: 0), for: .search)
         searchBar.setPositionAdjustment(UIOffset(horizontal: 6, vertical: 0), for: .clear)
+        /// 文字
+        searchBar.searchTextPositionAdjustment = UIOffset.init(horizontal: 3, vertical: 0)
         searchBar.layer.cornerRadius = 15
-        searchBar.layer.masksToBounds = true
     }
     
 }
@@ -37,6 +49,23 @@ extension BatteryAddressSearchViewController: UISearchBarDelegate {
     
     func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
         self.tableView.reloadSections(IndexSet(arrayLiteral: 0), with: .none)
+        /// 更改地址搜索页面状态
+        self.tableViewLeadingConstraint.constant = 8
+        self.tableViewHeightConstraint.constant = self.mapView.bounds.height - 8 - (self.view.bounds.height * self.tableViewHeightConstraint.multiplier)
+        tableView.layer.cornerRadius = 4
+        UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 0.8, initialSpringVelocity: 0, options: [], animations: {
+            self.view.layoutIfNeeded()
+        }, completion: nil)
+    }
+    
+    func searchBarTextDidEndEditing(_ searchBar: UISearchBar) {
+        searchBar.resignFirstResponder()
+        
+        self.tableViewLeadingConstraint.constant = 0
+        self.tableViewHeightConstraint.constant = 0
+        UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 0.8, initialSpringVelocity: 0, options: [], animations: {
+            self.view.layoutIfNeeded()
+        }, completion: nil)
     }
     
 }
@@ -51,7 +80,7 @@ extension BatteryAddressSearchViewController: UITableViewDelegate, UITableViewDa
         if section == 0 {
             return 1
         } else {
-            return 5
+            return 15
         }
     }
 
@@ -64,6 +93,10 @@ extension BatteryAddressSearchViewController: UITableViewDelegate, UITableViewDa
             cell.configure(input: searchBar.text ?? "")
             return cell
         }
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        searchBar.resignFirstResponder()
     }
     
 }
